30b64284ca2d7d8930995350d86bd2b6ad4b5b5b
Tyler Hancock <darklime@live.ca>
2019-06-21 20:10:06 -0600

    Added crafting recipes.

--- src/main/java/net/darkhax/msmlegacy/MSMLegacy.java
+++ src/main/java/net/darkhax/msmlegacy/MSMLegacy.java
@@ -3,10 +3,14 @@ package net.darkhax.msmlegacy;
 import java.io.File;
 
 import net.darkhax.bookshelf.registry.RegistryHelper;
+import net.darkhax.bookshelf.util.OreDictUtils;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.Enchantment.Rarity;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -65,42 +69,52 @@ public class MSMLegacy {
         this.dawnStar = this.registry.registerItem(new ItemSword(this.config.getMaterial("dawn_star", 3, 1286, 8, 6, 22)), "dawn_star");
         this.ignite = this.registry.registerEnchantment(this.config.getSwordEnchantment("ignite", this.dawnStar, Rarity.COMMON, 1, 3), "ignite");
         this.sparks = this.registry.registerEnchantment(this.config.getSwordEnchantment("sparks", this.dawnStar, Rarity.RARE, 1, 1), "sparks");
-
+        this.registry.addShapedRecipe("dawn_star", new ItemStack(dawnStar), "bcm", "cmc", "rcb", 'b', Items.BLAZE_POWDER, 'c', Items.FIRE_CHARGE, 'm', Items.MAGMA_CREAM, 'r', Items.BLAZE_ROD);
+        
         this.vampiricBlade = this.registry.registerItem(new ItemSword(this.config.getMaterial("vampiric_blade", 3, 812, 8, 7, 12)), "vampiric_blade");
         this.feast = this.registry.registerEnchantment(this.config.getSwordEnchantment("feast", this.vampiricBlade, Rarity.COMMON, 1, 3), "feast");
         this.vitality = this.registry.registerEnchantment(this.config.getSwordEnchantment("vitality", this.vampiricBlade, Rarity.RARE, 1, 1), "vitality");
-
+        this.registry.addShapedRecipe("vampiric_blade", new ItemStack(vampiricBlade), " ir", "ori", "so ", 'i', OreDictUtils.INGOT_IRON, 'r', OreDictUtils.DUST_REDSTONE, 'o', OreDictUtils.OBSIDIAN, 's', OreDictUtils.STICK_WOOD);
+        
         this.gladiolus = this.registry.registerItem(new ItemSword(this.config.getMaterial("gladiolus", 3, 645, 8, 6, 10)), "gladiolus");
         this.venomousAspect = this.registry.registerEnchantment(this.config.getSwordEnchantment("venomous_aspect", this.gladiolus, Rarity.COMMON, 1, 3), "venomous_aspect");
         this.absorb = this.registry.registerEnchantment(this.config.getSwordEnchantment("absorb", this.gladiolus, Rarity.RARE, 1, 1), "absorb");
+        this.registry.addShapedRecipe("gladiolus", new ItemStack(gladiolus), " lv", "sfl", "ts ", 'l', OreDictUtils.TREE_LEAVES, 'v', OreDictUtils.VINE, 's', OreDictUtils.TREE_SAPLING, 'f', new ItemStack(Blocks.RED_FLOWER, 1, 1), 't', OreDictUtils.STICK_WOOD);
 
         this.draconicBlade = this.registry.registerItem(new ItemSword(this.config.getMaterial("draconic_blade", 3, 1089, 8, 7, 16)), "draconic_blade");
         this.keenEdge = this.registry.registerEnchantment(this.config.getSwordEnchantment("keen_edge", this.draconicBlade, Rarity.COMMON, 1, 3), "keen_edge");
         this.scorn = this.registry.registerEnchantment(this.config.getSwordEnchantment("scorn", this.draconicBlade, Rarity.RARE, 1, 1), "scorn");
+        this.registry.addShapedRecipe("draconic_blade", new ItemStack(draconicBlade), " ir", "ldi", "sl ", 'i', OreDictUtils.INGOT_IRON, 'r', OreDictUtils.DUST_REDSTONE, 'l', OreDictUtils.GEM_LAPIS, 'd', OreDictUtils.GEM_DIAMOND, 's', OreDictUtils.STICK_WOOD);
 
         this.eyeEndBlade = this.registry.registerItem(new ItemSword(this.config.getMaterial("eye_end_blade", 3, 1580, 8, 8, 22)), "eye_end_blade");
         this.enderPulse = this.registry.registerEnchantment(this.config.getSwordEnchantment("ender_pulse", this.eyeEndBlade, Rarity.COMMON, 1, 3), "ender_pulse");
         this.enderAura = this.registry.registerEnchantment(this.config.getSwordEnchantment("ender_aura", this.eyeEndBlade, Rarity.RARE, 1, 1), "ender_aura");
+        this.registry.addShapedRecipe("eye_end_blade", new ItemStack(eyeEndBlade), " po", "dep", "sd ", 'p', OreDictUtils.ENDERPEARL, 'o', OreDictUtils.OBSIDIAN, 'd', OreDictUtils.GEM_DIAMOND, 'e', Items.ENDER_EYE, 's', OreDictUtils.STICK_WOOD);
 
         this.crystalineBlade = this.registry.registerItem(new ItemSword(this.config.getMaterial("crystaline_blade", 3, 570, 8, 5, 28)), "crystaline_blade");
         this.greed = this.registry.registerEnchantment(this.config.getSwordEnchantment("greed", this.crystalineBlade, Rarity.COMMON, 1, 3), "greed");
         this.wisdom = this.registry.registerEnchantment(this.config.getSwordEnchantment("wisdom", this.crystalineBlade, Rarity.RARE, 1, 1), "wisdom");
+        this.registry.addShapedRecipe("crystaline", new ItemStack(crystalineBlade), " gg", "qpg", "sq ", 'g', OreDictUtils.BLOCK_GLASS, 'p', OreDictUtils.PANE_GLASS, 'q', OreDictUtils.GEM_QUARTZ, 's', OreDictUtils.STICK_WOOD);
 
         this.glacialBlade = this.registry.registerItem(new ItemSword(this.config.getMaterial("glacial_blade", 3, 680, 8, 6, 15)), "glacial_blade");
         this.frozenAspect = this.registry.registerEnchantment(this.config.getSwordEnchantment("frozen_aspect", this.glacialBlade, Rarity.COMMON, 1, 3), "frozen_aspect");
         this.frostWave = this.registry.registerEnchantment(this.config.getSwordEnchantment("frost_wave", this.glacialBlade, Rarity.RARE, 1, 1), "frost_wave");
+        this.registry.addShapedRecipe("glacial", new ItemStack(glacialBlade), " ip", "ipi", "si ", 'i', Blocks.ICE, 'p', Blocks.PACKED_ICE, 's', OreDictUtils.STICK_WOOD);
 
         this.aeithersGuard = this.registry.registerItem(new ItemSword(this.config.getMaterial("aethers_guard", 3, 1796, 8, 8, 22)), "aethers_guard");
         this.ascension = this.registry.registerEnchantment(this.config.getSwordEnchantment("ascension", this.aeithersGuard, Rarity.COMMON, 1, 3), "ascension");
         this.descension = this.registry.registerEnchantment(this.config.getSwordEnchantment("descension", this.aeithersGuard, Rarity.RARE, 1, 1), "descension");
+        this.registry.addShapedRecipe("aethers_guard", new ItemStack(aeithersGuard), "fdg", "dgi", "sif", 'f', OreDictUtils.FEATHER, 'd', OreDictUtils.GEM_DIAMOND, 'g', OreDictUtils.GLOWSTONE, 'i', OreDictUtils.INGOT_IRON, 's', OreDictUtils.STICK_WOOD);
 
         this.withersBane = this.registry.registerItem(new ItemSword(this.config.getMaterial("wither_bane", 3, 1869, 8, 6, 16)), "wither_bane");
         this.decay = this.registry.registerEnchantment(this.config.getSwordEnchantment("decay", this.withersBane, Rarity.COMMON, 1, 3), "decay");
         this.consumingShadows = this.registry.registerEnchantment(this.config.getSwordEnchantment("consuming_shadows", this.withersBane, Rarity.RARE, 1, 1), "consuming_shadows");
+        this.registry.addShapedRecipe("wither_bane", new ItemStack(withersBane), " ss", "qns", "kq ", 's', Blocks.SOUL_SAND, 'q', OreDictUtils.GEM_QUARTZ, 'n', OreDictUtils.NETHER_STAR, 'k', OreDictUtils.STICK_WOOD);
 
         this.adminiumArk = this.registry.registerItem(new ItemSword(this.config.getMaterial("adminium_ark", 3, 9999999, 8, 99999, 999)), "adminium_ark");
         this.stealth = this.registry.registerEnchantment(this.config.getSwordEnchantment("stealth", this.adminiumArk, Rarity.COMMON, 1, 3), "stealth");
         this.extinction = this.registry.registerEnchantment(this.config.getSwordEnchantment("extinction", this.adminiumArk, Rarity.RARE, 1, 1), "extinction");
+        this.registry.addShapedRecipe("adminium_ark", new ItemStack(adminiumArk), " bb", "fcb", "sf ", 'b', Blocks.BEDROCK, 'f', Blocks.END_PORTAL_FRAME, 'c', Blocks.COMMAND_BLOCK, 's', OreDictUtils.STICK_WOOD);
 
         this.config.syncConfigData();
     }
